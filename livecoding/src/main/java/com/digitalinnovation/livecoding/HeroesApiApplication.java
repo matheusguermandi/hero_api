99d524687e49c76fbad62e421ae443cf2c6f9432
matheusguermandi <matheus_guermandi@hotmail.com>
2021-03-16 14:34:42 -0300

    add: hero api

---
+++ livecoding/src/main/java/com/digitalinnovation/livecoding/HeroesApiApplication.java
@@ -0,0 +1,15 @@
+package com.digitalinnovation.livecoding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.socialsignin.spring.data.dynamodb.repository.config.EnableDynamoDBRepositories;
+
+@SpringBootApplication
+@EnableDynamoDBRepositories
+public class HeroesApiApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(HeroesApiApplication.class, args);
+  }
+
+}
